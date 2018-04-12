0e6175bf8bc8639c35c2bd4989390d0edaa13ef0
Oleksandr Bonar <oleksandr.bonar@outlook.com>
2018-04-12 18:17:14 +0300

    Spring Boot Initialize

---
+++ src/main/java/com/reserved/spring/ReservedApplication.java
@@ -0,0 +1,12 @@
+package com.reserved.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReservedApplication.class, args);
+	}
+
+}
